3e64dd05a54a25ee00e64de3f2f8f67cb1d5bd72
TheDeathlyCow <thedeathlycow@gmail.com>
2024-04-09 20:07:04 -1000

    some basic infrastructure

--- src/main/java/com/github/thedeathlycow/thermoo/patches/ThermooPatches.java
+++ src/main/java/com/github/thedeathlycow/thermoo/patches/ThermooPatches.java
@@ -2,21 +2,58 @@ package com.github.thedeathlycow.thermoo.patches;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+
 public class ThermooPatches implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("thermoo-patches");
+    public static final String MODID = "thermoo-patches";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    @Override
+    public void onInitialize() {
+        logPatchMods();
+    }
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    private static void logPatchMods() {
+        var builder = new StringBuilder();
+        Arrays.stream(IntegratedMod.values())
+                .filter(IntegratedMod::isModLoaded)
+                .forEach(
+                        mod -> {
+                            builder.append('\n');
+                            builder.append(" - ");
+                            builder.append(mod.id);
+                        }
+                );
+
+
+        String loadedMods = builder.toString();
+        if (loadedMods.isEmpty()) {
+            LOGGER.info("No Thermoo patches available for current mod set.");
+        } else {
+            LOGGER.info("Initialized Thermoo Patches for the following mods: {}", loadedMods);
+        }
+    }
+
+    public enum IntegratedMod {
+
+        ARMOR_POINTS_PP("armorpointspp");
+
+        private final String id;
+
+        IntegratedMod(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public boolean isModLoaded() {
+            return FabricLoader.getInstance().isModLoaded(id);
+        }
+    }
 }
