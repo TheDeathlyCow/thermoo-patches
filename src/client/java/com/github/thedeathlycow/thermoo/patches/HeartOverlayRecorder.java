addec697ea8f5cf33709d5d2f075997ee07c5f47
Terrails <github@sl.terrails.com>
2024-06-16 01:10:34 +0200

    switch over to builtin events from colorful hearts

--- src/client/java/com/github/thedeathlycow/thermoo/patches/HeartOverlayRecorder.java
+++ src/client/java/com/github/thedeathlycow/thermoo/patches/HeartOverlayRecorder.java
@@ -1,8 +1,6 @@
 package com.github.thedeathlycow.thermoo.patches;
 
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Util;
-import org.jetbrains.annotations.Nullable;
 import org.joml.Vector2i;
 
 import java.util.Arrays;
@@ -12,9 +10,6 @@ public class HeartOverlayRecorder {
     public static final HeartOverlayRecorder INSTANCE = new HeartOverlayRecorder();
 
     private static final int MAX_OVERLAY_HEARTS = 20;
-
-    private PlayerEntity player;
-    private int displayHealth;
 
     private final Vector2i[] heartPositions = Util.make(() -> {
         var positions = new Vector2i[MAX_OVERLAY_HEARTS];
@@ -30,31 +25,6 @@ public class HeartOverlayRecorder {
 
     public Vector2i[] getHeartPositions() {
         return heartPositions;
-    }
-
-    /**
-     * Extra information not given in colorful hearts api
-     * @return Returns the current player
-     */
-    @Nullable
-    public PlayerEntity getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(PlayerEntity player) {
-        this.player = player;
-    }
-
-    /**
-     * Extra information not given in colorful hearts api
-     * @return Returns the current player's max display health
-     */
-    public int getDisplayHealth() {
-        return displayHealth;
-    }
-
-    public void setDisplayHealth(int displayHealth) {
-        this.displayHealth = displayHealth;
     }
 
     private HeartOverlayRecorder() {
--- src/client/java/com/github/thedeathlycow/thermoo/patches/colorfulhearts/ColorfulHeartsEventListeners.java
+++ src/client/java/com/github/thedeathlycow/thermoo/patches/colorfulhearts/ColorfulHeartsEventListeners.java
@@ -4,35 +4,32 @@ import com.github.thedeathlycow.thermoo.api.client.StatusBarOverlayRenderEvents;
 import com.github.thedeathlycow.thermoo.patches.HeartOverlayRecorder;
 import net.minecraft.entity.player.PlayerEntity;
 import org.joml.Vector2i;
-import terrails.colorfulhearts.fabric.api.event.FabHeartEvents;
+import terrails.colorfulhearts.api.fabric.ColorfulHeartsApi;
+import terrails.colorfulhearts.api.fabric.event.FabHeartEvents;
 
-public class ColorfulHeartsEventListeners {
+public class ColorfulHeartsEventListeners implements ColorfulHeartsApi {
 
-    public static void register() {
-        FabHeartEvents.POST_RENDER.register(post -> {
-            var recorder = HeartOverlayRecorder.INSTANCE;
-
-            Vector2i[] positions = recorder.getHeartPositions();
-
-            PlayerEntity player = recorder.getPlayer();
-            if (player != null) {
-                int displayHealth = recorder.getDisplayHealth();
-
-                StatusBarOverlayRenderEvents.AFTER_HEALTH_BAR
-                        .invoker()
-                        .render(
-                                post.getGuiGraphics(),
-                                player,
-                                positions,
-                                displayHealth,
-                                20
-                        );
-            }
+    public ColorfulHeartsEventListeners() {
+        FabHeartEvents.SINGLE_RENDER.register(event -> {
+            HeartOverlayRecorder.INSTANCE.setHeartPosition(event.getIndex(), event.getX(), event.getY());
         });
-    }
 
-    private ColorfulHeartsEventListeners() {
+        FabHeartEvents.POST_RENDER.register(post -> {
+            PlayerEntity player = post.getPlayer();
+            int displayHealth = post.getDisplayHealth();
 
+            Vector2i[] positions = HeartOverlayRecorder.INSTANCE.getHeartPositions();
+
+            StatusBarOverlayRenderEvents.AFTER_HEALTH_BAR
+                    .invoker()
+                    .render(
+                            post.getGuiGraphics(),
+                            player,
+                            positions,
+                            displayHealth,
+                            20
+                    );
+        });
     }
 
 }
