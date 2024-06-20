c17e2e1d3276a737d47aff9febb9d5c44b06cb58
Filip <github@sl.terrails.com>
2024-06-20 08:49:16 +0200

    switch over to builtin events from colorful hearts (#9)

--- src/client/java/com/github/thedeathlycow/thermoo/patches/ThermooPatchesClient.java
+++ src/client/java/com/github/thedeathlycow/thermoo/patches/ThermooPatchesClient.java
@@ -1,14 +1,11 @@
 package com.github.thedeathlycow.thermoo.patches;
 
-import com.github.thedeathlycow.thermoo.patches.colorfulhearts.ColorfulHeartsEventListeners;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ThermooPatchesClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        if (IntegratedMod.COLORFUL_HEARTS.isModLoaded()) {
-            ColorfulHeartsEventListeners.register();
-        }
+
     }
 }
