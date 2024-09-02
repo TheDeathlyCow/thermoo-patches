e5c844ec984a7fd3e0016c2b41d7ab3a4fe34a54
TheDeathlyCow <thedeathlycow@gmail.com>
2024-09-01 21:04:33 -1000

    remove extra and mob origins from integrated mod enum

--- src/main/java/com/github/thedeathlycow/thermoo/patches/IntegratedMod.java
+++ src/main/java/com/github/thedeathlycow/thermoo/patches/IntegratedMod.java
@@ -11,9 +11,8 @@ public enum IntegratedMod {
     IMMERSIVE_WEATHERING("immersive_weathering", "https://modrinth.com/mod/immersive-weathering"),
     FABRIC_SEASONS("seasons", "https://modrinth.com/mod/fabric-seasons"),
     SERENE_SEASONS("sereneseasons", "https://modrinth.com/mod/serene-seasons"),
-    ORIGINS("origins", "https://modrinth.com/mod/origins"),
-    MOB_ORIGINS("moborigins", "https://modrinth.com/mod/moborigins"),
-    EXTRA_ORIGINS("extraorigins", "https://modrinth.com/mod/extra-origins");
+    ORIGINS("origins", "https://modrinth.com/mod/origins");
+    
     private final String id;
 
     private final String modpage;
