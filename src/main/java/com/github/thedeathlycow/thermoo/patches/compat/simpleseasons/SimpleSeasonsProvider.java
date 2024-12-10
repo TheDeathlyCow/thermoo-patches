35ce7ddfbf46555476c051ec8592acd54e9d518d
TheDeathlyCow <thedeathlycow@gmail.com>
2024-12-10 01:35:08 -1000

    add tropical seasons provider

--- src/main/java/com/github/thedeathlycow/thermoo/patches/compat/simpleseasons/SimpleSeasonsProvider.java
+++ src/main/java/com/github/thedeathlycow/thermoo/patches/compat/simpleseasons/SimpleSeasonsProvider.java
@@ -5,6 +5,8 @@ import com.github.thedeathlycow.thermoo.api.season.ThermooSeasonEvents;
 import com.github.thedeathlycow.thermoo.patches.IntegratedMod;
 import io.github.steveplays28.simpleseasons.api.SimpleSeasonsApi;
 import io.github.steveplays28.simpleseasons.state.world.SeasonTracker;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.biome.Biome;
 
 import java.util.Optional;
 
@@ -13,6 +15,10 @@ public final class SimpleSeasonsProvider {
     public static void registerSeasonProviderEvent() {
         if (IntegratedMod.SIMPLE_SEASONS.isModLoaded()) {
             ThermooSeasonEvents.GET_CURRENT_SEASON.register(world -> {
+                if (!SimpleSeasonsApi.worldHasSeasons(world)) {
+                    return Optional.empty();
+                }
+
                 SeasonTracker.Seasons season = SimpleSeasonsApi.getSeason(world);
                 return Optional.ofNullable(
                         switch (season) {
@@ -23,6 +29,23 @@ public final class SimpleSeasonsProvider {
                             default -> null;
                         }
                 );
+            });
+
+            ThermooSeasonEvents.GET_CURRENT_TROPICAL_SEASON.register((world, pos) -> {
+                RegistryEntry<Biome> biome = world.getBiome(pos);
+                if (SimpleSeasonsApi.biomeHasWetAndDrySeasons(biome)) {
+                    SeasonTracker.Seasons season = SimpleSeasonsApi.getSeason(world);
+
+                    return Optional.ofNullable(
+                            switch (season) {
+                                case SUMMER, SPRING -> ThermooSeason.TROPICAL_WET;
+                                case WINTER, FALL -> ThermooSeason.TROPICAL_DRY;
+                                default -> null;
+                            }
+                    );
+                }
+
+                return Optional.empty();
             });
         }
     }
