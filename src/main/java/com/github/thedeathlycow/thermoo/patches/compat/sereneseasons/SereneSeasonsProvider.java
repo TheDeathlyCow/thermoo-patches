8c17001d7aaafa1ecfa0cbb6454fa48f7108404d
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-02 19:34:31 -1000

    add serene seasons dependency

---
+++ src/main/java/com/github/thedeathlycow/thermoo/patches/compat/sereneseasons/SereneSeasonsProvider.java
@@ -0,0 +1,15 @@
+package com.github.thedeathlycow.thermoo.patches.compat.sereneseasons;
+
+import com.github.thedeathlycow.thermoo.patches.IntegratedMod;
+
+public final class SereneSeasonsProvider {
+    public static void initialize() {
+        if (IntegratedMod.SERENE_SEASONS.isModLoaded()) {
+
+        }
+    }
+
+    private SereneSeasonsProvider() {
+
+    }
+}
