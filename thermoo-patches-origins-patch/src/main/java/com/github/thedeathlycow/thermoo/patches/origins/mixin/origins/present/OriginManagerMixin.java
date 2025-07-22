b7b0bb63360945a05022f34dfc9a53ecab6e1294
TheDeathlyCow <thedeathlycow@gmail.com>
2025-07-22 00:16:10 -1000

    configure a java dependency for origins

---
+++ thermoo-patches-origins-patch/src/main/java/com/github/thedeathlycow/thermoo/patches/origins/mixin/origins/present/OriginManagerMixin.java
@@ -0,0 +1,8 @@
+package com.github.thedeathlycow.thermoo.patches.origins.mixin.origins.present;
+
+import io.github.apace100.origins.origin.OriginManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(OriginManager.class)
+public class OriginManagerMixin {
+}
