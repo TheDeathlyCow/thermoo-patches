9629b89d0772fdc5f39fec31ee9f791d0c55fc34
TheDeathlyCow <thedeathlycow@gmail.com>
2026-01-31 02:52:56 -1000

    fix typo in mod json

--- thermoo-patches-base/src/client/java/com/github/thedeathlycow/thermoo/patches/client/ThermooPatchesModMenu.java
+++ thermoo-patches-base/src/client/java/com/github/thedeathlycow/thermoo/patches/client/ThermooPatchesModMenu.java
@@ -3,7 +3,6 @@ package com.github.thedeathlycow.thermoo.patches.client;
 import com.github.thedeathlycow.thermoo.patches.config.ThermooPatchesConfig;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ThermooPatchesModMenu implements ModMenuApi {
