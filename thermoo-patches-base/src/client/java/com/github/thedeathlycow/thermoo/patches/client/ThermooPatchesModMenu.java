d108f575c6fd5fd4689e515303934082c0140973
TheDeathlyCow <thedeathlycow@gmail.com>
2026-01-31 02:49:41 -1000

    fix auto config deprecated warning

--- thermoo-patches-base/src/client/java/com/github/thedeathlycow/thermoo/patches/client/ThermooPatchesModMenu.java
+++ thermoo-patches-base/src/client/java/com/github/thedeathlycow/thermoo/patches/client/ThermooPatchesModMenu.java
@@ -4,12 +4,13 @@ import com.github.thedeathlycow.thermoo.patches.config.ThermooPatchesConfig;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ThermooPatchesModMenu implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ThermooPatchesConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(ThermooPatchesConfig.class, parent).get();
     }
 
 }
